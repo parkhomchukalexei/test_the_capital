89b1171e12e891b620cfcb7b7808fb8bc49dfafd
parkhomchukalexei <parkhomchuk.alexei@gmail.com>
2024-02-22 19:47:10 +0200

    v1

--- matrix_converter/core/views.py
+++ matrix_converter/core/views.py
@@ -8,16 +8,8 @@ class MatrixController(APIView):
 
     def get(self, request):
 
-        url_matrix = [asyncio.run(get_matrix(SOURCE_URL))]
+        data = [{'matrix_field': matrix} for matrix in [asyncio.run(get_matrix(SOURCE_URL)), TRAVERSAL, TRAVERSAL_2]]
 
-        test_matrix = [TRAVERSAL]
-
-        one_more_test_matrix = [TRAVERSAL_2]
-        data = [
-            {'matrix_field': url_matrix},
-            {'matrix_field': test_matrix},
-            {'matrix_field': one_more_test_matrix},
-        ]
         serializer = MatrixSerializer(data=data, many=True)
 
         if serializer.is_valid():
